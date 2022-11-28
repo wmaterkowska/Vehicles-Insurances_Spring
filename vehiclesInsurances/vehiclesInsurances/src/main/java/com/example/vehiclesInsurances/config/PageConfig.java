a0742271b822bc0572b02b1cff20945bdd8d83d3
Weronika Materkowska <w.m.materkowska@gmail.com>
2022-11-28 01:08:57 +0100

    controller not finished

---
+++ vehiclesInsurances/vehiclesInsurances/src/main/java/com/example/vehiclesInsurances/config/PageConfig.java
@@ -0,0 +1,19 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class PageConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/listOfVehicles").setViewName("listOfVehicles");
+        registry.addViewController("/").setViewName("index");
+    }
+
+
+
+
+}
